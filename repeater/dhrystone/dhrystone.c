d3c6027564e8468364572589b4eb0e2eb7d29623
Keguang Zhang <keguang.zhang@unisoc.com>
2019-03-28 20:05:23 +0800

    repeater: prj.conf: use -O2 optimization by default
    Use -O2 optimization by default.
    
    Signed-off-by: Keguang Zhang <keguang.zhang@unisoc.com>

--- repeater/dhrystone/dhrystone.c
+++ repeater/dhrystone/dhrystone.c
@@ -6,12 +6,12 @@ int dhry_main(const struct shell *shell, size_t argc, char **argv)
 	return 0;
 }
 
-SHELL_CREATE_STATIC_SUBCMD_SET(dhry_commands) {
+SHELL_STATIC_SUBCMD_SET_CREATE(dhry_commands,
 	SHELL_CMD(NULL, NULL,
 	 NULL,
 	 dhry_main),
 	SHELL_SUBCMD_SET_END
-};
+);
 
 SHELL_CMD_REGISTER(dhrystone, &dhry_commands, "dhrystone benchmark", dhry_main);
 
